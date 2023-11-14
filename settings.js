1b01991b7de1fb4f21f1647762c714f9e44ae8ca
Clintonlelop <132855603+Clintonlelop@users.noreply.github.com>
2023-11-14 22:01:56 +0100

    Add files via upload

--- settings.js
+++ settings.js
@@ -2,15 +2,16 @@ const chalk = require("chalk")
 const fs = require("fs")
 
 //aumto presence update
-global.autoTyping = false //auto tying in gc (true to on, false to off)
-global.autoRecord = false //auto recording (true to on, false to off)
-global.autoblockmorroco = false //auto block 212 (true to on, false to off)
-global.autokickmorroco = false //auto kick 212 (true to on, false to off) 
-global.antispam = false //auto kick spammer (true to on, false to off)
+global.autoReading = false //auto recording (true to on, false to off)
+global.autoTyping = true //auto tying (true to on, false to off)
+global.autoRecord = true //auto recording (true to on, false to off)
+global.autoblockmorroco = true //auto block 212 (true to on, false to off)
+global.autokickmorroco = true //auto kick 212 (true to on, false to off) 
+global.antispam = true //auto kick spammer (true to on, false to off)
 
 
 //if api key expire, u can generate one from here: https://beta.openai.com/account/api-keys
-global.keyopenai = "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"
+global.keyopenai = "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"
 
 //documents variants
 global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
@@ -21,27 +22,26 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.ownernomer = "2349024925375" //ur owner number2
+global.ownername = "𝙇𝞢𝙇𝞗𝞠" //ur owner name
+global.ytname = "YT: Clintonlelop" //ur yt chanel name
+global.socialm = "GitHub: Clintonlelop" //ur github or insta name
+global.location = "Nigeria, Enugu, Enugu" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
+global.botname = "𝘾𝙇𝞘𝞜𝙏𝞗𝞜 𝞑𝞗𝙏"
+global.ownernumber = '2349024925375'
+global.ownername = '𝙇𝞢𝙇𝞗𝞠'
+global.ownerNumber = ["2349024925375@s.whatsapp.net"]
 global.ownerweb = "https://youtube.com/@DGXeon"
-global.websitex = "https://youtu.be/L_SIk59QeAU"
-global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
-global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
+global.websitex = "https://youtube.com/@DGXeon"
+global.wagc = "https://chat.whatsapp.com/GAjcbK8MBw7JtmsAYyEdse"
+global.themeemoji = '🗿'
+global.wm = "𝘾𝙇𝞘𝞜𝙏𝞗𝞜 𝞑𝞗𝙏"
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
-global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n"
-global.creator = "916909137213@s.whatsapp.net"
+global.packname = "𝘾𝙇𝞘𝞜𝙏𝞗𝞜"
+global.author = "𝞑𝞗𝙏"
+global.creator = "2349024925375@s.whatsapp.net"
 global.prefa = ['','!','.','#','&']
 global.sessionName = 'session'
 global.hituet = 0
